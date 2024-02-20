1b99f6530dc62991326a10ffec9aba522c6e94eb
Jezuz <Jezuz@DESKTOP-RN4DP0B>
2024-02-20 01:54:12 -0600

    Agregar archivos de proyecto.

---
+++ Empleado/Empleado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empleado
+{
+    internal class Empleado
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Puesto { get; set; }
+        private double salario;
+
+        public Empleado(string nombre, int edad, string puesto)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Puesto = puesto;
+            salario = 0;
+        }
+
+        public void EstablecerSalario(double nuevoSalario)
+        {
+            salario = nuevoSalario;
+        }
+
+       
+        public double ObtenerSalario()
+        {
+            return salario;
+        }
+        public void ImprimirInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Edad: {Edad}");
+            Console.WriteLine($"Puesto: {Puesto}");
+            Console.WriteLine($"Salario: {salario:C}");
+        }
+    }
+}
---
+++ Empleado/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Empleado
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Empleado empleado1 = new Empleado("Juan", 30, "Desarrollador");
+
+           
+            empleado1.EstablecerSalario(30000);
+
+            
+            empleado1.ImprimirInformacion();
+
+            
+            Console.ReadLine();
+        }
+    }
+}
